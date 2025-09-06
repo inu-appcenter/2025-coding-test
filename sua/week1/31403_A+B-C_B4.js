8cb5e5a00742e7c4fc111e39da3eeac94ba13866
ssuasu <kamijsa2004@naver.com>
2025-09-06 12:54:25 +0900

    feat : 10250_ACM호텔_B3

--- sua/week1/31403_A+B-C_B4.js
+++ sua/week1/31403_A+B-C_B4.js
@@ -1,13 +1,26 @@
 const fs = require("fs");
-const input = fs
-  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
-  .toString()
-  .trim()
-  .split(" ")
-  .map(Number);
+const path = process.platform === "linux" ? "/dev/stdin" : "Wiki\\input.txt";
+const [a, b, c] = fs.readFileSync(path).toString().trim().split("\n");
 
-const [a, b, c] = input;
-const [d, e, f] = input.map(String);
-
+console.log(Number(a) + Number(b) - Number(c));
 console.log(a + b - c);
-console.log(d + e - f);
+
+/*내가 썼던 코드. 계속 틀렸다고 함
+근데 위의 코드는 바로 정답.
+왜?
+아... split를 \n으로 해야 했다. 
+입력이 줄바꿈으로 되어 있었기에...*/
+
+// const fs = require("fs");
+// const input = fs
+//   .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+//   .toString()
+//   .trim()
+//   .split(" ")
+//   .map(Number);
+
+// const [a, b, c] = input;
+// const [d, e, f] = [a,b,c].map(String);
+
+// console.log(a + b - c);
+// console.log(d + e - f);
