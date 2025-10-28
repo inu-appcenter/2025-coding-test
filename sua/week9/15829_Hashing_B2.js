5298c8b2fd26d8696f0115d5c8420dac136e9e90
ssuasu <kamijsa2004@naver.com>
2025-10-29 00:23:38 +0900

    feat : 15829_Hashing_B2

---
+++ sua/week9/15829_Hashing_B2.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const path = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(path).toString().trim().split("\n");
+
+let T = Number(input[0]); // length of string
+// console.log("a".charCodeAt());
+
+let idx = 0;
+let sum = 0;
+let r = 1;
+while (T--) {
+  sum += ((input[1][idx].charCodeAt() - "a".charCodeAt() + 1) * r) % 1234567891;
+  //   console.log(double);
+  r = (r * 31) % 1234567891;
+  idx++;
+}
+
+console.log(sum);
