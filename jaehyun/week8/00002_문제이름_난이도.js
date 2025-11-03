8d51dc5784bb30c5ea98a0bf857a1fe4eb733f81
LimJaeHyun <guswogus81@gmail.com>
2025-11-03 17:17:15 +0900

    feat : 2630 색종이 만들기 S2

---
+++ jaehyun/week8/00002_문제이름_난이도.js
@@ -0,0 +1,11 @@
+// 문제링크 :
+
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+function Solution(input) {}
+
+console.log(Solution(input));
